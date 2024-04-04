662498eadd030b68115a1ca592115e88f2b8321e
Deon Okonkwo <deondazy@gmail.com>
2024-04-04 20:41:50 +0100

    Refactor NullFormatterTest class to use Denosys\BooBoo namespace

--- tests/unit/src/Formatter/NullFormatterTest.php
+++ tests/unit/src/Formatter/NullFormatterTest.php
@@ -1,11 +1,13 @@
 <?php
 
-use League\BooBoo\Formatter;
+use Denosys\BooBoo\Formatter;
 use PHPUnit\Framework\TestCase;
 
-class NullFormatterTest extends TestCase {
+class NullFormatterTest extends TestCase
+{
 
-    public function testNoResponseForAnyException() {
+    public function testNoResponseForAnyException()
+    {
         $formatter = new Formatter\NullFormatter();
         $this->assertNull($formatter->format(new \Exception));
         $this->assertNull($formatter->format(new \ErrorException));
